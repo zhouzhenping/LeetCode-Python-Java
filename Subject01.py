301a39b4016f70a712f874a60c1f97facc1f7f37
zhouzhenping <674253885@qq.com>
2020-02-09 02:58:38 +0800

    新添加两个python方法跑object01两数之和

--- Subject01.py
+++ Subject01.py
@@ -7,15 +7,39 @@
 所以返回 [0, 1]
 '''
 class Solution01:
-    def twosum01(self, nums: 'List[int]', target: 'int') -> 'List[int]':
+    def twoSum01(self, nums: 'List[int]', target: 'int') -> 'List[int]':
         dict = {}
         for i, num in enumerate(nums): # enumerate() 函数用于将一个可遍历的数据对象（如列表、元组或字符串）组合成为一个索引序列，
         # 同时列出数据和数据下表，一般用在for循环中。i 和 num, 默认前面一个是索引，后面一个是对象的内容
+            #print('before enmumerate: %d %d %d' %(i, num, dict[num]))
             dict[num] = i # 键值对，num是键，i是值，赋予给num键
 
         for i, num in enumerate(nums):
-            if target - num in dict and i != dict[target - num]: # 是判断键（key)在不在dict里面
+            if target - num in dict and i != dict[target - num]: # 是判断键（key)在不在dict里面,且不等于自身
                 return [i, dict [target - num]]
+    
+    def twoSum02(self, nums, target):
+        '''
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        '''
+        record = dict()
+        for i in range(len(nums)):
+            if nums[i] in record:
+                return [record[nums[i]], i]
+            record[target - nums[i]] = i
+        return [-1, -1]
+
+    def twoSum03(self, nums: 'List[int]', target: int) -> 'List[int]':
+        d = {}
+        for i in range(len(nums)):
+            t = target - nums[i]
+            if t in d.keys():
+                return([d[t], i])
+            d[nums[i]] = i,
+    
+    
 
     def main(self):
         print ('main method')
@@ -26,14 +50,6 @@ if __name__ == "__main__":
 nums = [2, 7, 11, 15]
 target = 9
 solution01 = Solution01()
-print(nums)
-print(target)
-result = solution01.twosum01(nums, target)
-result02 = solution01.main() 
-print('start')
-print(result)
-#print(Solution01.twosum01(nums, target))
-#twosum(nums, target)
-print('new change')
-print('second change')
-
+#print(solution01.twoSum01(nums, target))
+#print(solution01.twoSum02(nums, target))
+print(solution01.twoSum03(nums, target))
