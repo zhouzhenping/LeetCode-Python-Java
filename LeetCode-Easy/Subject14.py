6923e7ccd797f45a3a5b30a0ce43d7b4e979cd20
zhouzhenping <674253885@qq.com>
2020-05-01 11:17:48 +0800

    2020-05.01 系统重装更新（劳动最光荣）

--- LeetCode-Easy/Subject14.py
+++ LeetCode-Easy/Subject14.py
@@ -1,6 +1,16 @@
-class Subject14:
+class Subject14(object):
     def longerstCommonPrefix01(self, strs: 'List[str]') -> 'str':
         if len(strs) == 0 or '' in strs:
             return ''
         if len(set(strs)) == 1:
-            
+            return strs[0]
+        maxlen = min([len(e) for e in strs])
+        check = 1
+        i = 0
+        while (check != 0 & i < maxlen):
+            sub = [e[:i] for e in strs]
+            if len(set(sub)) == 1:
+                i += 1
+            else:
+                check = 0
+        return strs[0][:i-1]
--- LeetCode-Easy/Subject14Solution.java
+++ LeetCode-Easy/Subject14Solution.java
@@ -1,13 +1,15 @@
 public class Subject14Solution {
 
     public static void main(String args[]) {
-        // TODO
+        Subject14Solution subject14Solution = new Subject14Solution();
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(subject14Solution.longestCommonPrefix04(strs));
     }
 
     /**
      * 查找最长公共前缀，水平扫描
      * 
-     * @param strs 字符串数据
+     * @param strs 字符串数组
      * @return 最长公共前缀
      */
     public String longestCommonPrefix01(String[] strs) {
@@ -26,6 +28,12 @@ public class Subject14Solution {
         return prefix;
     }
 
+    /**
+     * 查找最长公共前缀，水平扫描
+     * 
+     * @param strs 字符串数组
+     * @return 最长公共前缀
+     */
     public String longestCommonPrefix02(String[] strs) {
         if (null == strs || strs.length == 0) {
             return "";
@@ -39,5 +47,77 @@ public class Subject14Solution {
             }
         }
         return strs[0];
+    }
+
+    /**
+     * 分治法
+     * 
+     * @param strs 字符串数组
+     * @return 最长公共前缀
+     */
+    public String longestCommanPrefix03(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        return longestCommonPrefix03(strs, 0, strs.length - 1);
+    }
+
+    private String longestCommonPrefix03(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[1];
+        } else {
+            int mid = (1 + r) / 2;
+            String lcpLeft = longestCommonPrefix03(strs, l, mid);
+            String lcpRight = longestCommonPrefix03(strs, mid + 1, r);
+            return commonPrefix03(lcpLeft, lcpRight);
+        }
+
+    }
+
+    String commonPrefix03(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return left.substring(0, i);
+            }
+        }
+        return left.substring(0, min);
+    }
+
+    /**
+     * 二分查找法
+     * 
+     * @param strs 字符串数组
+     * @return 最长公共前缀
+     */
+    public String longestCommonPrefix04(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLen = Integer.MAX_VALUE;
+        for (String str: strs) {
+            minLen = Math.min(minLen, str.length());
+        }
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (isCommonPrefix04(strs, middle)) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
+        }
+        return strs[0].substring(0, (low + high) / 2);
+    }
+
+    private boolean isCommonPrefix04(String[] strs, int len) {
+        String str1 = strs[0].substring(0, len);
+        for (int i = 1; i < strs.length; i++) {
+            if (!strs[i].startsWith(str1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
